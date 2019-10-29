a82c601f627f1a406896abd3fbb12098cefb0ddf
Özsel Kılınç <ozsel@ozsel-mac.wifi-campus.warwick.ac.uk>
2019-10-29 00:48:55 +0000

    removing unused parts of the code

--- olbr/her_sampler.py
+++ olbr/her_sampler.py
@@ -39,13 +39,7 @@ def make_sample_her_transitions(replay_strategy, replay_k, reward_fun):
         # Replace goal with achieved goal but only for the previously-selected
         # HER transitions (as defined by her_indexes). For the other transitions,
         # keep the original goal.
-        #palm_ind_her = transitions['info_palm'][her_indexes].copy()
-        #palm_ind = palm_ind_her[:,0] == 1.0
         future_ag = episode_batch['ag'][episode_idxs[her_indexes], future_t]
-        #transitions_g_copy = transitions['g'][her_indexes].copy()
-        #future_ag_copy = future_ag.copy()
-        #transitions_g_copy[palm_ind] = future_ag_copy[palm_ind]
-        #transitions['g'][her_indexes] = transitions_g_copy
         transitions['g'][her_indexes] = future_ag
 
         # Reconstruct info dictionary for reward  computation.
@@ -232,24 +226,11 @@ def make_sample_her_transitions_multiobj(replay_strategy, replay_k, reward_fun, 
         # Replace goal with achieved goal but only for the previously-selected
         # HER transitions (as defined by her_indexes). For the other transitions,
         # keep the original goal.
-        ##nb_object = 4
-        #import pdb; pdb.set_trace()
         current_g = transitions['g'][her_indexes].copy().reshape(-1,nb_object,target_size)
         future_ag = episode_batch['ag'][episode_idxs[her_indexes], future_t].copy().reshape(-1,nb_object,target_size)
         for i_object in range(nb_object):
             random_ind = np.where(np.random.uniform(0,1,len(future_ag)) < her_prob) #np.random.permutation(len(future_ag))[0:len(future_ag)//2]
             current_g[random_ind,i_object,:] = future_ag[random_ind,i_object,:].copy()
-        #random_ind = np.random.permutation(len(future_ag))
-        #nb_subsets = 2**nb_object - 1
-        #subsets = findsubsets(nb_object)
-        #portion_len = len(future_ag)//nb_subsets
-        #for i_subset in range(nb_subsets):
-        #    random_ind_portion = random_ind[i_subset*portion_len:(i_subset+1)*portion_len]
-        #    current_g_portion = current_g[random_ind_portion,].copy()
-        #    future_ag_portion = future_ag[random_ind_portion,].copy()
-        #    current_g_portion[:,subsets[i_subset],:] = future_ag_portion[:,subsets[i_subset],:].copy()
-        #    current_g[random_ind_portion,] = current_g_portion
-
         transitions['g'][her_indexes] = current_g.reshape(-1,nb_object*target_size)
 
         # Reconstruct info dictionary for reward  computation.
@@ -271,12 +252,3 @@ def make_sample_her_transitions_multiobj(replay_strategy, replay_k, reward_fun, 
         return transitions
 
     return _sample_her_transitions
-
-import itertools 
-  
-def findsubsets(nb_object):
-    mainset = list(range(nb_object))
-    subsets = []
-    for i in range(1,nb_object+1):
-        subsets.extend(list(itertools.combinations(mainset, i)))
-    return subsets
