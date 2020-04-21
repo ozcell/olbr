d8ed8d6260f70fc47b8d26a456ae8e694bcad4d5
Özsel Kılınç <ozsel@ozsel-mac.local>
2020-04-21 20:56:39 +0100

    debegging

--- olbr/main_q_v2_hard.py
+++ olbr/main_q_v2_hard.py
@@ -61,12 +61,12 @@ def init(config, agent='robot', her=False, object_Qfunc=None,
                                     obj_action_type=config['obj_action_type'], 
                                     observe_obj_grp=config['observe_obj_grp'],
                                     obj_range=config['obj_range'])
-        envs = SubprocVecEnv([make_env(ENV_NAME, i_env, 'Fetch', agent == 'object') for i_env in range(N_ENVS)])
+        envs = SubprocVecEnv([make_env(ENV_NAME, i_env, 'Fetch') for i_env in range(N_ENVS)])
         if 'Dense' in ENV_NAME:
-            envs_test = SubprocVecEnv([make_env('FetchPickAndPlaceMultiDense-v1', i_env, 'Fetch', agent == 'object') for i_env in range(N_ENVS)])
+            envs_test = SubprocVecEnv([make_env('FetchPickAndPlaceMultiDense-v1', i_env, 'Fetch') for i_env in range(N_ENVS)])
         else:
-            envs_test = SubprocVecEnv([make_env('FetchPickAndPlaceMulti-v1', i_env, 'Fetch', agent == 'object') for i_env in range(N_ENVS)])
-        envs_render = SubprocVecEnv([make_env(ENV_NAME, i_env, 'Fetch', agent == 'object') for i_env in range(1)])
+            envs_test = SubprocVecEnv([make_env('FetchPickAndPlaceMulti-v1', i_env, 'Fetch') for i_env in range(N_ENVS)])
+        envs_render = SubprocVecEnv([make_env(ENV_NAME, i_env, 'Fetch') for i_env in range(1)])
         n_rob_actions = 4
         n_actions = config['max_nb_objects'] * len(config['obj_action_type']) + n_rob_actions
     elif 'Fetch' in ENV_NAME and 'Multi' in ENV_NAME and 'Flex' not in ENV_NAME:
