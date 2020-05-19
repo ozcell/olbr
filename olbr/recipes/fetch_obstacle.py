714ed264a62c264cda1fa8546f79b85d19a434d2
Özsel Kılınç <ozsel@ozsel-mac.local>
2020-05-19 12:52:03 +0100

    adding pnp normal to the recipe

--- olbr/recipes/fetch_obstacle.py
+++ olbr/recipes/fetch_obstacle.py
@@ -48,6 +48,8 @@ elif exp_config['env'] == 'PnPHardest':
      env_name_list = ['FetchPickAndPlaceHardestMulti{}-v1'.format(suffix)]
 elif exp_config['env'] == 'PnPInsertion':
      env_name_list = ['FetchPickAndPlaceInsertionMulti{}-v1'.format(suffix)]
+elif exp_config['env'] == 'PnPNormal':
+     env_name_list = ['FetchPickAndPlaceMulti{}-v1'.format(suffix)]
 elif exp_config['env'] == 'All':
      env_name_list = ['FetchPushObstacleSideGapMulti{}-v1'.format(suffix), 
                       'FetchPushObstacleMiddleGapMulti{}-v1'.format(suffix), 
@@ -55,7 +57,9 @@ elif exp_config['env'] == 'All':
                       'FetchPickAndPlaceShelfMulti{}-v1'.format(suffix), 
                       'FetchPickAndPlaceObstacleMulti{}-v1'.format(suffix),
                       'FetchPickAndPlaceHardestMulti{}-v1'.format(suffix),
-                      'FetchPickAndPlaceInsertionMulti{}-v1'.format(suffix)]
+                      'FetchPickAndPlaceInsertionMulti{}-v1'.format(suffix),
+                      'FetchPickAndPlaceMulti{}-v1'.format(suffix)
+                      ]
 
 for env_name in env_name_list:
 
@@ -125,6 +129,8 @@ for env_name in env_name_list:
                 path = './ObT_models/obj/pnp_hardest_7d_ep25/'
             elif env_name == 'FetchPickAndPlaceInsertionMulti-v1':
                 path = './ObT_models/obj/pnp_insertion_7d_ep25/'
+            elif env_name == 'FetchPickAndPlaceMulti-v1':
+                path = './ObT_models/obj/pnp_hardest_7d_ep25/'
 
             print('loading object model')
             print(path)
